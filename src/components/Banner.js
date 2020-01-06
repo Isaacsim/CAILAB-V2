ca87ce51f648bb99f36aacbb27dd691117352cd4
Isaac Sim <dltkr34@naver.com>
2020-01-06 21:42:13 +0900

    init

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,20 +1,29 @@
 import React from 'react'
 
-const Banner = (props) => (
-    <section id="banner" className="major">
-        <div className="inner">
-            <header className="major">
-                <h1>Hi, my name is Forty</h1>
-            </header>
-            <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
-                <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
-                </ul>
-            </div>
-        </div>
-    </section>
+const Banner = props => (
+  <section id="banner" className="major">
+    <div className="inner">
+      <header className="major">
+        <h1>Communications and Artificial Intelligent Laboratory - CAI Lab.</h1>
+      </header>
+      <div className="content">
+        <p>
+          연구실 간략한 소개를 작성 연구실 간략한 소개를 작성 연구실 간략한
+          소개를 작성
+          <br />
+          연구실 간략한 소개를 작성 연구실 간략한 소개를 작성 연구실 간략한
+          소개를 작성
+        </p>
+        <ul className="actions">
+          <li>
+            <a href="#one" className="button next scrolly">
+              Learn more
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
 )
 
 export default Banner
--- src/pages/landing.js
+++ src/pages/landing.js
@@ -7,78 +7,93 @@ import BannerLanding from '../components/BannerLanding'
 import pic08 from '../assets/images/pic08.jpg'
 import pic09 from '../assets/images/pic09.jpg'
 import pic10 from '../assets/images/pic10.jpg'
+import peopleJinYoungKim from '../assets/images/JinYoungKim.png'
+import peopleIsaacSim from '../assets/images/IsaacSim.jpg'
 
-const Landing = (props) => (
-    <Layout>
-        <Helmet>
-            <title>Landing - Forty by HTML5 UP</title>
-            <meta name="description" content="Landing Page" />
-        </Helmet>
+const Landing = props => (
+  <Layout>
+    <Helmet>
+      <title>Landing - Forty by HTML5 UP</title>
+      <meta name="description" content="Landing Page" />
+    </Helmet>
 
-        <BannerLanding />
+    <BannerLanding />
 
-        <div id="main">
-            <section id="one">
-                <div className="inner">
-                    <header className="major">
-                        <h2>Sed amet aliquam</h2>
-                    </header>
-                    <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna.</p>
-                </div>
-            </section>
-            <section id="two" className="spotlights">
-                <section>
-                    <Link to="/generic" className="image">
-                        <img src={pic08} alt="" />
-                    </Link>
-                    <div className="content">
-                        <div className="inner">
-                            <header className="major">
-                                <h3>Orci maecenas</h3>
-                            </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
-                            <ul className="actions">
-                                <li><Link to="/generic" className="button">Learn more</Link></li>
-                            </ul>
-                        </div>
-                    </div>
-                </section>
-                <section>
-                    <Link to="/generic" className="image">
-                        <img src={pic09} alt="" />
-                    </Link>
-                    <div className="content">
-                        <div className="inner">
-                            <header className="major">
-                                <h3>Rhoncus magna</h3>
-                            </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
-                            <ul className="actions">
-                                <li><Link to="/generic" className="button">Learn more</Link></li>
-                            </ul>
-                        </div>
-                    </div>
-                </section>
-                <section>
-                    <Link to="/generic" className="image">
-                        <img src={pic10} alt="" />
-                    </Link>
-                    <div className="content">
-                        <div className="inner">
-                            <header className="major">
-                                <h3>Sed nunc ligula</h3>
-                            </header>
-                            <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
-                            <ul className="actions">
-                                <li><Link to="/generic" className="button">Learn more</Link></li>
-                            </ul>
-                        </div>
-                    </div>
-                </section>
-            </section>
+    <div id="main">
+      <section id="one">
+        <div className="inner">
+          <header className="major">
+            <h2>People</h2>
+          </header>
+          <p>연구실에 함께하고있는 사람들에 대한 정보입니다.</p>
         </div>
-
-    </Layout>
+      </section>
+      <section id="two" className="spotlights">
+        <section>
+          <Link to="/generic" className="image">
+            <img src={peopleJinYoungKim} alt="" />
+          </Link>
+          <div className="content">
+            <div className="inner">
+              <header className="major">
+                <h3>Jin Young Kim</h3>
+                <h4>Professor</h4>
+              </header>
+              <p>바이오그래피 작성</p>
+              <ul className="actions">
+                <li>
+                  <Link to="/generic" className="button">
+                    Action button (의견 제시 요망)
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+        <section>
+          <Link to="/generic" className="image">
+            <img src={peopleIsaacSim} alt="" />
+          </Link>
+          <div className="content">
+            <div className="inner">
+              <header className="major">
+                <h3>Isaac Sim</h3>
+                <h4>Ph.D. Candidates</h4>
+              </header>
+              <p>바이오그래피 작성</p>
+              <ul className="actions">
+                <li>
+                  <Link to="/generic" className="button">
+                    Action button (의견 제시 요망)
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+        <section>
+          <Link to="/generic" className="image">
+            <img src={pic10} alt="" />
+          </Link>
+          <div className="content">
+            <div className="inner">
+              <header className="major">
+                <h3>Sed nunc ligula</h3>
+              </header>
+              <p>바이오그래피 작성</p>
+              <ul className="actions">
+                <li>
+                  <Link to="/generic" className="button">
+                    Action button (의견 제시 요망)
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </section>
+    </div>
+  </Layout>
 )
 
 export default Landing
